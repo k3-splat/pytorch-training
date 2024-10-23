990d848b771ccdeadf2b9307cc04893ca0cb6d0e
k3-splat <6322045@ed.tus.ac.jp>
2024-10-23 14:11:46 +0900

    ajjd

---
+++ 01/main.py
@@ -0,0 +1,6 @@
+from utils.function import add
+
+if __name__ == "__main__":
+    print("Entry Point")
+    n = add(2, 3)
+    print(n)
---
+++ 01/utils/function.py
@@ -0,0 +1,3 @@
+def add(x, y):
+    ret = x + y
+    return ret
